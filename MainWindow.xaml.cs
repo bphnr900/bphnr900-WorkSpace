bbea1b709cbce0bf1a33ac5a966eb55df5fdd657
bphnr900 <40142616+bphnr900@users.noreply.github.com>
2018-06-10 22:07:39 +0900

    MyClock

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MyClock
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+        void timer_Tick(object sender,EventArgs e)
+        {
+            this.textBlock1.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
